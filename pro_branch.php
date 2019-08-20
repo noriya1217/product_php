5748cc8d3419bd4085bd8a9b26b2376b0ecc6da9
noriya <noriya.saeki@gmail.com>
2019-08-21 01:15:40 +0900

    product first commit add/edit

---
+++ pro_branch.php
@@ -0,0 +1,33 @@
+<?php
+if (isset($_POST['edit']) == true) {
+    if (isset($_POST['procode']) == false) {
+        header('Location: pro_ng.php');
+        exit();
+    } else {
+        $pro_code = $_POST['procode'];
+        header('Location: pro_edit.php?procode='.$pro_code);
+        exit();
+    }
+} elseif (isset($_POST['delete']) == true) {
+    if (isset($_POST['procode']) == false) {
+        header('Location: pro_ng.php');
+        exit();
+    } else {
+        $pro_code = $_POST['procode'];
+        header('Location: pro_delete.php?procode='.$pro_code);
+        exit();
+    }
+} elseif (isset($_POST['disp']) == true) {
+    if (isset($_POST['procode']) == false) {
+        header('Location: pro_ng.php');
+        exit();
+    } else {
+        $pro_code = $_POST['procode'];
+        header('Location: pro_disp.php?procode='.$pro_code);
+        exit();
+    }
+} elseif (isset($_POST['add']) == true) {
+    header('Location: pro_add.php');
+    exit();
+}
+?>
